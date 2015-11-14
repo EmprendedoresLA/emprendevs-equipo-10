8fb30fa036bc0db398b076fedf897ff595f994b7
piranimarcos <marcos.pir94@gmail.com>
2015-11-14 17:57:28 -0300

    creando rutas

--- routes/main.js
+++ routes/main.js
@@ -5,23 +5,22 @@ app.get('/video/:vid', function(req, res){
     var v = new Videos({ video: req.params.vid });
     v.save(function(err, doc){
         if(!err){
-            res.redirect('ver/' + doc.video + '/' + doc.id);
+            res.redirect('/video/ver/' + doc.video + '/' + doc.id);
         } else {
             res.end(err);    
         }    
     });
 });
 
+
 app.get('/video/ver/:vid/:id', function(req, res){
     Videos.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
             if (doc.video == req.params.vid){
-                console.log("lo encontro");
+                res.redirect('/');
             }else{
-                res.redirect('ver/' + doc.video + '/' + doc.id);
+                res.redirect('/video/ver/' + doc.video + '/' + doc.id);
             }
-
-            //res.redire('edit', { title: 'Edit', person: doc});
         } else {
             res.end(err);    
         }    
